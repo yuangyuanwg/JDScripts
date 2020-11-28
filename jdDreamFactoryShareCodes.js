c9416b316ad931f38b9b5cab8ff0914010e17623
Tersd07 <Tersd07@users.noreply.github.com>
2020-11-28 21:36:10 +0800

    update

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -4,7 +4,7 @@
 支持京东N个账号
  */
 //云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
-// github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 JXFACTORY_SHARECODES(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
+// github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 PLANT_BEAN_SHARECODES(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
@@ -12,17 +12,17 @@ let shareCodes = [
   '',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有东东萌宠互助码
-if (process.env.JXFACTORY_SHARECODES) {
-  if (process.env.JXFACTORY_SHARECODES.indexOf('&') > -1) {
+if (process.env.DREAM_FACTORY_SHARE_CODES) {
+  if (process.env.DREAM_FACTORY_SHARE_CODES.indexOf('&') > -1) {
     console.log(`您的互助码选择的是用&隔开\n`)
-    shareCodes = process.env.JXFACTORY_SHARECODES.split('&');
-  } else if (process.env.JXFACTORY_SHARECODES.indexOf('\n') > -1) {
+    shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split('&');
+  } else if (process.env.DREAM_FACTORY_SHARE_CODES.indexOf('\n') > -1) {
     console.log(`您的互助码选择的是用换行隔开\n`)
-    shareCodes = process.env.JXFACTORY_SHARECODES.split('\n');
+    shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split('\n');
   } else {
-    shareCodes = process.env.JXFACTORY_SHARECODES.split();
+    shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split();
   }
-} else if (process.env.JD_COOKIE) {
+} else if (process.env.DREAM_FACTORY_SHARE_CODES) {
   console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < shareCodes.length; i++) {
